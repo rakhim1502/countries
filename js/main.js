d3ff19daa5d9d6792edbdfca26f78aecfe92ebed
rakhim1502 <uzbrm95@gmail.com>
2024-07-05 12:14:59 +0500

    Add dark theme js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const elSiteThemeToggler = document.querySelector('.dark-mode-button');
-
-elSiteThemeToggler.addEventListener('click', function () {
-
-  document.body.classList.toggle('dark-mode');
-
+document.addEventListener("DOMContentLoaded", function() {
+  const currentTheme = localStorage.getItem("theme");
+  document.body.classList.add(currentTheme + "-mode");
 });
+
+function toggleTheme() {
+  if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
+  }
+  else {
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
+  }
+}
+
+document.getElementById("toggle-theme").addEventListener("click", toggleTheme);
